327c4426818e81688fe834eeeca7daed1534bf94
Mohsen Janlou <Mohsen.mahmoudjanlou@gmail.com>
2022-07-13 15:30:10 +0430

    Add Monthly and make it optional.

--- year-calendar.js
+++ year-calendar.js
@@ -1,18 +1,21 @@
 (function($) {
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    //const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    //const WEEKDAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    // });
 
     $.fn.Calendar = function(options) {
         var settings = $.extend({
             // These are the defaults.
             year: "2022",
+            month: 1,
+            yearly: true,
             displayYear: 8,
             date: []
         }, options);
-
-        var str = calendarFunc(this.attr('id'), settings.year, settings.date, settings.displayYear);
+        
+        var str = "";
+        if(settings.yearly)
+            str = calendarFunc(this.attr('id'), settings.year, settings.date, settings.displayYear);
+        else
+            str = monthcalendarFunc(this.attr('id'), settings.year, settings.date, settings.month);
 
         return this.html(str);
     };
@@ -20,8 +23,6 @@
     function calendarFunc(e, year, date, displayYear) {
         var xDate = JSON.stringify(date);
         xDate = xDate.replaceAll("\"", "'");
-        //alert(xDate);
-        //date = date.map(x => "'" + x + "'").toString();
 
         var str = "";
         str += "<div class=\"col-12 col-md-12 col-lg-12 text-center text-light bg-dark\"><ul class=\"ul\">";
@@ -93,6 +94,89 @@
             }
             str += "</tbody></table></div>";
         }
+        str += "</div>";
+        str += "<div class=\"modal fade\" id=\"CalModal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"exampleModalLabel\" aria-hidden=\"true\">" +
+            "<div class=\"modal-dialog\" role=\"document\"><div class=\"modal-content\"><div class=\"modal-header\">" +
+            "<h5 class=\"modal-title\" id=\"dataTitle\">title</h5>" +
+            "<button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">" +
+            "<span aria-hidden=\"true\">&times;</span></button></div>" +
+            "<div class=\"modal-body\" id=\"dataText\"></div><div class=\"modal-footer\">" +
+            "<button type=\"button\" class=\"btn btn-secondary\" data-dismiss=\"modal\">Close</button>" +
+            "</div></div></div></div>";
+        return str;
+    }
+
+    function monthcalendarFunc(e, year, date,month) {
+        var xDate = JSON.stringify(date);
+        xDate = xDate.replaceAll("\"", "'");
+
+        var str = "";
+        str += "<div id=\"YearTitle\" class=\"col-12 col-md-12 col-lg-12 text-center text-light bg-dark\">" +
+            "<h3>" + year + "</h3></div>";
+        m = month - 1;
+            
+            str += "<div class=\"col-12 col-md-12 col-lg-12\"><div class=\"row\"><div class=\"col-12 col-md-12 col-lg-12 text-center text-light bg-dark\"><h5>" + months[m] + "</h5></div></div>" +
+                "<table class=\"table align-items-center table-flush table-hover dataTable\" id=\"dataTableHover\" role=\"grid\" aria-describedby=\"dataTableHover_info\">" +
+                "<thead class=\"thead-light\"><tr role=\"row\"><th >sun</th><th >Mon</th><th >Tue</th>" +
+                "<th >Wed</th><th >Thu</th><th >Fri</th>" +
+                "<th >Sat</th></tr></thead><tbody>";
+
+            var day = 1;
+            const dy = new Date(year + '/' + (m + 1) + '/' + day);
+            var startDay = dy.getDay();
+
+            var dim = daysInMonth((m + 1), year);
+
+            var start = false;
+            for (var w = 0; w < 6; w++) {
+                str += "<tr role=\"row\" class=\"odd\">";
+                for (var d = 0; d < 7; d++) {
+                    if (startDay == d) {
+                        start = true;
+                    }
+                    if (start) {
+                        if (day <= dim) {
+                            var mc = m + 1;
+                            var ms = mc.toString();
+                            if (mc < 10) {
+                                ms = '0' + ms;
+                            }
+
+                            var ds = day.toString();
+                            if (day < 10) {
+                                ds = '0' + ds;
+                            }
+                            var isDate = false;
+                            var bg = "light";
+                            var fg = "light";
+                            var title = "";
+                            date.forEach(element => {
+                                if (element.date == (year + "-" + ms + "-" + ds)) {
+                                    isDate = true;
+                                    bg = element.color;
+                                    title = " onclick=\"showMoreData('" + element.title + "','" + element.text + "');\"";
+                                }
+                            });
+                            if (bg.toLowerCase() == "light" || bg.toLowerCase() == "warning") {
+                                fg = "dark";
+                            } else {
+                                fg = "light";
+                            }
+                            // if (isDate) {
+                            str += "<td class=\"rounded text-" + fg + " bg-" + bg + "\" " + title + ">" + day + "</td>";
+
+                            day++;
+                        } else {
+                            str += "<td class=\"sorting_1\"></td>";
+                        }
+                    } else {
+                        str += "<td class=\"sorting_1\"></td>";
+                    }
+                }
+                str += "</tr>";
+            }
+            str += "</tbody></table></div>";
+        
         str += "</div>";
         str += "<div class=\"modal fade\" id=\"CalModal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"exampleModalLabel\" aria-hidden=\"true\">" +
             "<div class=\"modal-dialog\" role=\"document\"><div class=\"modal-content\"><div class=\"modal-header\">" +
